9d9c714a0b3dda42521ad1883194a102c7317d40
veniamintul <tulupovvenya@yandex.ru>
2022-01-31 22:09:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var a=10;
+console.log(a);
+
+
+const workers = [
+    {"name":"John","salary":500},
+    {"name":"Mike","salary":1300},
+    {"name":"Linda","salary":1500}];
+
+  console.log(getWorthyWorkers(workers))
